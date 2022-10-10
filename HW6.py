6fdcc57e17d64b91c9bd0fb023f6c89450434171
milesi <irina-rxv@yandex.ru>
2022-10-10 21:25:35 +0300

    Дз 5 сделано

---
+++ HW6.py
@@ -0,0 +1,15 @@
+# import math
+#
+# a = input('введите число ')
+# array = []
+# while a != "":
+#     array.append(int(a))
+#     a = input('введите число ')
+# print(math.gcd(*array))
+
+
+# strk = 'ООООРРРРОРОРРРРРРРРООРОРОРРРРРРРРРРРРРРРРРРРРРРРРРРРРРРР'
+# print(len(max(strk.split("О"))))
+
+
+
